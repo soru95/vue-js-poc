9ab60c3fcc93894aa442f094cc9fbb1574ab65f2
Saurabh M Agrawal <50284437+soru95@users.noreply.github.com>
2022-10-06 10:54:30 +0800

    Initial commit

---
+++ App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1>Custom Vue.js Select Component</h1>
+    <CustomSelect
+      :options="['go', 'python', 'rust', 'javascript']"
+      :default="'go'"
+      class="select"
+      @input="alert(displayToKey($event))"
+    />
+  </div>
+</template>
+
+<script>
+import CustomSelect from "./components/CustomSelect.vue";
+
+export default {
+  components: {
+    CustomSelect,
+  },
+};
+</script>
+
+<style lang="scss">
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+#app {
+  max-width: 400px;
+  margin: 0 auto;
+  line-height: 1.4;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
